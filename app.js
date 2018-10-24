ade8a659eb86fe186251803bf976a6ed28ca56e5
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2018-10-24 14:09:54 +0530

    pause to Queryselector

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ GAME RULES:
 
 */
 
-var scores,roundScore, activePlayer;
+
+
+var scores,roundScore, activePlayer,dice;
 
 scores = [0,0];
 roundScore=0;
+activePlayer=1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;   // by using active player we will switch the players current-0 & current-1
+//document.querySelector('#current-'+activePlayer).innerHTML = '<em>' + dice + '</em>';
+ 
+
 
